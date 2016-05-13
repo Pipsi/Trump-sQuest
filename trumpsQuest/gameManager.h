38c5663f6b45a191bb05d30292a64792825e7328
pearcelong96 <wowpearce@hotmail.co.uk>
2016-05-13 20:06:52 +0100

    Started working on gameManager.h

--- trumpsQuest/gameManager.h
+++ trumpsQuest/gameManager.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef gameManager_h
+#define gameManager_h
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class gameManager
+{
+
+protected:
+
+	string world[5][5];
+
+
+public:
+	gameManager(); //constructor
+	~gameManager(); 
+
+
+
+
+
+};
+
+#endif 
+
+
